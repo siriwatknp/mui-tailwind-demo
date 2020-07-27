0a90c4f39ab30ffa224da1f4f68e3fe558afcd73
siriwatknp <siriwatkunaporn@gmail.com>
2020-07-27 12:18:00 +0700

    feat: 🎸 add demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React from 'react';
+import { StylesProvider, makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from "@material-ui/core/Typography";
+
+import Apps from '@material-ui/icons/Apps';
 import logo from './logo.svg';
 import './App.css';
 
+const useStyles = makeStyles({
+  logo: {
+    width: '3rem',
+    height: '3rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  avatarBtn: {
+    padding: '0.25rem',
+    marginLeft: '0.25rem',
+  },
+  wrapper: { // I need to come up with a name here
+    display: 'flex',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+  }
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StylesProvider injectFirst>
+      <AppBar position={'relative'} color={'default'} elevation={0}>
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.wrapper}>
+            <img src={logo} className={`App-logo ${classes.logo}`} alt="logo" />
+            <Typography variant="h1" color="primary" className={classes.title}>Material-UI</Typography>
+          </div>
+          <div>
+            <IconButton>
+              <Apps />
+            </IconButton>
+            <IconButton className={classes.avatarBtn}>
+              <Avatar>BM</Avatar>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+      <Divider />
+
+      <AppBar position={'relative'} color={'default'} elevation={0}>
+        <Toolbar className="justify-between">
+          <div className="flex items-center">
+            <img src={logo} className="App-logo h-12 w-12" alt="logo" />
+            <Typography variant="h1" className="text-2xl font-bold" color="primary">MUI Tailwind</Typography>
+          </div>
+          <div>
+            <IconButton>
+              <Apps />
+            </IconButton>
+            <IconButton className="p-1 ml-1">
+              <Avatar>BM</Avatar>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </StylesProvider>
   );
 }
 
